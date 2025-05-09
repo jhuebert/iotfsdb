304b14bac45c791abaaa9e41c4b6ad022cba7bdd
Jason Huebert <jason.huebert@candescent.com>
2025-05-09 16:26:16 -0500

    refactor: Update forEach and map methods to use Collection instead of Iterable and improve error handling in parallel processing

--- src/main/java/org/huebert/iotfsdb/service/ParallelUtil.java
+++ src/main/java/org/huebert/iotfsdb/service/ParallelUtil.java
@@ -1,41 +1,32 @@
 package org.huebert.iotfsdb.service;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class ParallelUtil {
 
-    public static <T> void forEach(Iterable<T> iterable, Consumer<T> consumer) {
-        map(iterable, item -> {
+    public static <T> void forEach(Collection<T> collection, Consumer<T> consumer) {
+        map(collection, item -> {
             consumer.accept(item);
             return null;
         });
     }
 
-    public static <I, O> List<O> map(Iterable<I> iterable, Function<I, O> mapper) {
+    public static <I, O> List<O> map(Collection<I> collection, Function<I, O> mapper) {
         try (ExecutorService es = Executors.newVirtualThreadPerTaskExecutor()) {
-
-            List<Future<O>> futures = new ArrayList<>();
-            for (I item : iterable) {
-                futures.add(es.submit(() -> mapper.apply(item)));
-            }
-
-            try {
-                List<O> results = new ArrayList<>(futures.size());
-                for (Future<O> future : futures) {
-                    results.add(future.get());
-                }
-                return results;
-            } catch (ExecutionException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
+            return collection.stream()
+                .map(item -> CompletableFuture.supplyAsync(() -> mapper.apply(item), es))
+                .toList()
+                .stream()
+                .map(CompletableFuture::join)
+                .toList();
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred during parallel processing", e);
         }
     }
 
