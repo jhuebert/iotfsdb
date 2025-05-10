6b037be8d1f00f215b8a99cca8283144654f49df
Jason Huebert <jasonhuebert@gmail.com>
2025-05-09 20:52:31 -0500

    Rename test method to accurately reflect tested functionality

--- src/test/java/org/huebert/iotfsdb/schema/SeriesFileTest.java
+++ src/test/java/org/huebert/iotfsdb/schema/SeriesFileTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SeriesFileTest {
 
     @Test
-    public void testGetIntervalDuration() {
+    public void testGetId() {
         SeriesDefinition definition = SeriesDefinition.builder().id("123").build();
         assertThat(SeriesFile.builder().definition(definition).build().getId()).isEqualTo("123");
     }
