667fd1180e9e576440a9a67560006d8dd3ebd6d4
Jason Huebert <jason.huebert@candescent.com>
2025-06-08 22:12:42 -0500

    Implement Float3Partition for Float3 type handling with null/Nan support

---
+++ src/main/java/org/huebert/iotfsdb/partition/Float3Partition.java
@@ -0,0 +1,32 @@
+package org.huebert.iotfsdb.partition;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+
+public class Float3Partition implements PartitionAdapter {
+
+    @Override
+    public int getTypeSize() {
+        return Float3.BYTES;
+    }
+
+    @Override
+    public Iterator<Number> getIterator(ByteBuffer buffer, int index, int length) {
+        return new BufferIterator<>(
+            buffer.slice(index * Float3.BYTES, length * Float3.BYTES),
+            this::readFloat3
+        );
+    }
+
+    private Number readFloat3(ByteBuffer buffer) {
+        Float3 value = Float3.readFrom(buffer);
+        return value.isNaN() ? null : value;
+    }
+
+    @Override
+    public void put(ByteBuffer buffer, int index, Number value) {
+        Float3 float3 = value == null ? Float3.NaN : Float3.fromDouble(value.doubleValue());
+        float3.writeTo(buffer.slice(index * Float3.BYTES, Float3.BYTES));
+    }
+
+}
